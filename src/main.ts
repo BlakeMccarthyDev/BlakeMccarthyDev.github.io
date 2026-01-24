2ec70ec3c527df2ccc3c58f1b2da89e0d1f7ff5a
BlakeMccarthyDev <blakemcc2022@gmail.com>
2026-01-24 02:57:32 +0000

    added plane mesh with shader setuo

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,57 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import * as THREE from "three";
+import { GUI } from "dat.gui";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import vertexshader from "./vertexShader.glsl.js"
+import fragshader from "./fragShader.glsl.js"
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
+camera.position.set(15, 15, 15)
+
+const gui = new GUI();
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enablePan = false
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+window.addEventListener("keydown", (e) => {
+  if (e.code === "ArrowRight")
+  {
+    console.log(e)
+  }
+})
+const fr = new FileReader()
+const geometry = new THREE.PlaneGeometry(15, 15, 15, 15);
+const material = new THREE.ShaderMaterial({
+  vertexShader: vertexshader,
+  fragmentShader: fragshader
+})
+
+
+const cube = new THREE.Mesh(geometry, material);
+cube.rotateOnAxis(new THREE.Vector3(1, 0, 0), -1.5725)
+scene.add(cube)
+
+scene.add(new THREE.AxesHelper(10));
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
