1c8f90f4baf2f92f1f3a3901dc887e96af733bef
BlakeMccarthyDev <blakemcc2022@gmail.com>
2026-01-26 23:31:01 +0000

    developed bg pattern + more

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,19 @@ import '../App.css'
 import Gallery from "../components/GalleryFeed"
 
 export default function Home() {
-    return(
+    return (
         <>
             <NavBar />
-            <div className="content">
+            <div className="home content">
                 <h1>Hello There.</h1>
                 <h2>I'm Blake, a developer from the UK.</h2>
                 <h3>Look on the right to scroll through some works I have made, or use the navbar buttons above. </h3>
-            </div>
+                <br />
+                <div className="gitwidgets">
+                    <img src="https://github-readme-stats.vercel.app/api/top-langs?username=BlakeMcCarthyDev&show_icons=true&locale=en&layout=compact&theme=merko&hide_border=true" alt="ovi" />
+                    <img src="https://github-contributions-api.deno.dev/blakemccarthydev.svg" className="graph" />
+                </div>
+            </div>  
             <Gallery />
         </>
     )
